1c9b876d5ee9ae370e2fc08dd05855f4e4b0a584
Robin <robin.leonard@gmail.com>
2022-08-12 14:04:57 +0100

    refactored card into component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,73 @@
+<script>
+	export let slug,
+						 coverImage,
+						 alt,
+						 coverWidth,
+						 coverHeight,
+						 title,
+						 excerpt,
+						 categories;
+</script>
+
+<article class="card">
+	<!--        todo refactor /work/ so not hard coded, i.e. linked to config.js-->
+	<a href="/work/{slug}">
+		<img
+			src={coverImage}
+			alt="{alt}"
+			width={coverWidth}
+			height={coverHeight}
+			style="ratio: {coverWidth} / {coverHeight}"
+		/>
+		<div class="card__copy">
+			<h2>{title}</h2>
+			<p>{excerpt}</p>
+			<span>{categories}</span>
+		</div>
+	</a>
+</article>
+
+<style lang="scss">
+
+
+
+
+	 article {
+      max-width: 291px;
+      margin: 0 vars.$xs6;
+      border-radius: vars.$xs;
+      background: white;
+
+     a { text-decoration: none; }
+     a:hover {text-decoration: underline; }
+
+      img {
+        border-top-left-radius: vars.$xs5;
+        border-top-right-radius: vars.$xs5;
+        margin-bottom: 0;
+        padding-bottom: 0;
+      }
+
+      h2 {
+        font-size: vars.$lg;
+      }
+
+      p {
+        font-size: vars.$base;
+      }
+
+      span {
+        font-size: vars.$sm;
+      }
+
+      .card__copy {
+        margin: 0 vars.$sm;
+        padding-bottom: vars.$base;
+
+        h2 {
+          margin-top: vars.$xs2;
+        }
+      }
+    }
+
+</style>
--- src/lib/components/PostsList.svelte
+++ src/lib/components/PostsList.svelte
@@ -1,30 +1,40 @@
 <script>
   export let posts = []
+  import Card from './Card.svelte';
 </script>
 
 
 <ul class="posts-list">
   {#each posts as post}
-    <li class="card">
-      <article>
-<!--        todo refactor so not hard coded-->
-        <a href="/work/{post.slug}">
-          <img
-          src={post.coverImage}
-          alt="{post.alt}"
-          width={post.coverWidth}
-          height={post.coverHeight}
-          style="ratio: {post.coverWidth} / {post.coverHeight}"
-          />
-          <div class="card__copy">
-            <h2>{post.title}</h2>
-            <p>{post.excerpt}</p>
-            <span>{post.categories}</span>
-          </div>
-        </a>
-      </article>
 
+    <li class="">
+      <Card slug="{post.slug}"
+            coverImage="{post.coverImage}"
+            alt="{post.alt}"
+            coverWidth="{post.coverWidth}"
+            coverHeight="{post.coverHeight}"
+            title="{post.title}"
+            excerpt="{post.excerpt}"
+            categories="{post.categories}"
+      />
 
+<!--      <article>-->
+<!--&lt;!&ndash;        todo refactor so not hard coded&ndash;&gt;-->
+<!--        <a href="/work/{post.slug}">-->
+<!--          <img-->
+<!--          src={post.coverImage}-->
+<!--          alt="{post.alt}"-->
+<!--          width={post.coverWidth}-->
+<!--          height={post.coverHeight}-->
+<!--          style="ratio: {post.coverWidth} / {post.coverHeight}"-->
+<!--          />-->
+<!--          <div class="card__copy">-->
+<!--            <h2>{post.title}</h2>-->
+<!--            <p>{post.excerpt}</p>-->
+<!--            <span>{post.categories}</span>-->
+<!--          </div>-->
+<!--        </a>-->
+<!--      </article>-->
     </li>
   {/each}
 </ul>
@@ -40,46 +50,5 @@
   justify-content: flex-start;
   list-style-type: none;
   padding: 0;
-
-
-  .card {
-    a {text-decoration: none;}
-    a:hover {text-decoration: underline; }
-
-    article {
-      max-width: 291px;
-      margin: 0 vars.$xs6;
-      border-radius: vars.$xs;
-      background: white;
-
-      img {
-        border-top-left-radius: vars.$xs5;
-        border-top-right-radius: vars.$xs5;
-        margin-bottom: 0;
-        padding-bottom: 0;
-      }
-
-      h2 {
-        font-size: vars.$lg;
-      }
-
-      p {
-        font-size: vars.$base;
-      }
-
-      span {
-        font-size: vars.$sm;
-      }
-
-      .card__copy {
-        margin: 0 vars.$sm;
-        padding-bottom: vars.$base;
-
-        h2 {
-          margin-top: vars.$xs2;
-        }
-      }
-    }
-  }
 }
 </style>
