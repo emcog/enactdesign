584cf0783668611842858070a99c590dd9c5707b
Robin <robin.leonard@gmail.com>
2022-11-15 14:00:13 +0000

    hack fix on global scoped styles of PicsForMD.svelte

--- src/lib/components/PicsForMd.svelte
+++ src/lib/components/PicsForMd.svelte
@@ -3,7 +3,7 @@
 	export let images = [{}];
 	export let hasBorder = false;
 	console.log('PicsForMd',images.length);
-	$: gridColumn = images.length === 1 ? "grid-column: 3/-1;": "";
+	// $: gridColumn = images.length === 1 ? "grid-column: 3/-1;": "";
 
 </script>
 
@@ -24,30 +24,31 @@
 
 <style lang="scss">
 
+
 	:global .post :where(figure.for-md) {
     display: grid;
     grid-column: 2/-1;
-    grid-gap: 0 vars.$xs6;
-    margin: 0 0 vars.$xl2 0;
+    //grid-gap: 0 vars.$xs6;
+    //margin: 0 0 vars.$xl2 0;
 
-    @media (min-width: vars.$for-tablet-portrait-up) {
-      grid-column: 2/-2;
-    }
-    @media (min-width: vars.$for-tablet-landscape-up) {
-      grid-column: 3/-3;
-    }
+    //@media (min-width: vars.$for-tablet-portrait-up) {
+    //  grid-column: 2/-2;
+    //}
+    //@media (min-width: vars.$for-tablet-landscape-up) {
+    //  grid-column: 3/-3;
+    //}
 
 
     figcaption {
       grid-column: 1/-1;
-      font-size: vars.$sm;
+      //font-size: vars.$sm;
       margin: 0;
-      padding: vars.$xs4;
+      //padding: vars.$xs4;
       background: var(--mid-grey);
 
-      @media (min-width: vars.$for-tablet-landscape-up) {
-        grid-column: 1/-1;
-      }
+      //@media (min-width: vars.$for-tablet-landscape-up) {
+      //  grid-column: 1/-1;
+      //}
     }
 
     img, video {
