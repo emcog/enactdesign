a0a5357243142f55c394c3e4f2cfc121f6a0562f
Robin <robin.leonard@gmail.com>
2022-11-15 11:54:38 +0000

    updates

--- src/routes/work/category/[category]/+page.svelte
+++ src/routes/work/category/[category]/+page.svelte
@@ -11,8 +11,14 @@
 	<title>Category: {data.category}</title>
 </svelte:head>
 
+<!--todo reinstate H1-->
+<!--<h1>Blog category: {data.category}</h1>-->
 
-<h1>Blog category: {data.category}</h1>
+<div class="category-col">
+	<h1 class="category-title">Category</h1>
+	<p>{data.category}</p>
+	<a class="" href="/work">all categories</a>
+</div>
 
 {#if data.posts.length}
 	<PostsList posts={data.posts} />
@@ -22,3 +28,68 @@
 
 	<p><a href="/blog">Back to blog</a></p>
 {/if}
+
+<style lang="scss">
+
+  div.category-col {
+    border-top: 3px solid var(--bright-green);
+    margin: 0 vars.$xs;
+    width: auto;
+
+    @media(min-width: vars.$for-tablet-portrait-up) {
+      min-width: auto;
+      margin: 0 vars.$xs 0 vars.$xs;
+    }
+
+    @media(min-width: vars.$for-tablet-landscape-up) {
+      grid-column: 1/4;
+      margin: 15px 0 vars.$base vars.$xl;
+      padding: vars.$xs3 0 0 0;
+    }
+  }
+
+  h1.category-title {
+    font-family: var(--headings);
+    font-size: vars.$base;
+    background: none;
+    width: auto;
+    background: var(--bright-green);
+    margin: vars.$xs6 0 vars.$sm 0;
+    padding-left: 0;
+    color: var(--grey);
+    background: none;
+
+    @media(min-width: vars.$for-tablet-landscape-up) {
+      //grid-column: 1/4;
+      //margin: 15px 0 0 vars.$xl;
+      //padding: vars.$base 0 0 0;
+    }
+  }
+
+  .category-col > p {
+    font-size: vars.$base;
+    margin: vars.$base 0 vars.$xs6 0;
+    padding: vars.$xs6;
+    text-transform: uppercase;
+    width: auto;
+    background: var(--bright-green);
+  }
+
+  a {
+    margin: vars.$xs6 vars.$xs6 vars.$xs6 0;
+    font-size: vars.$sm;
+    font-weight: 500;
+    letter-spacing: vars.$xs9;
+    text-transform: uppercase;
+    text-decoration: none;
+    background: var(--soft-green);
+    //display: block;
+    line-height: 1.25;
+    padding: vars.$xs3;
+  }
+
+  a:hover {
+    background: var(--bright-green);
+  }
+
+</style>
